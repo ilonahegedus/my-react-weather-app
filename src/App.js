90c9b7914261fa1883b38296ed4188347bef94dd
ilonahegedus <88673740+ilonahegedus@users.noreply.github.com>
2022-04-24 00:09:44 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App"> Weather App </div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        This project was coded by Ilona Hegedus and is{" "}
+        <a
+          href="https://github.com/ilonahegedus/my-react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHUb.
+        </a>
+      </footer>
+    </div>
+  );
 }
