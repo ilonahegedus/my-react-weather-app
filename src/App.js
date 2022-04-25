d40dee6175112b1a37d748b679e38da0b8de127b
ilonahegedus <88673740+ilonahegedus@users.noreply.github.com>
2022-04-25 09:12:07 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        This project was coded by Ilona Hegedus and is{" "}
-        <a
-          href="https://github.com/ilonahegedus/my-react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHUb.
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          This project was coded by Ilona Hegedus and is{" "}
+          <a
+            href="https://github.com/ilonahegedus/my-react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHUb.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
